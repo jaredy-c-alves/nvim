8d79acd17c61138bd93da4a041545cbd22bf4057
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-09-17 09:22:40 +0100

    Adding initial settings

---
+++ lua/settings.lua
@@ -0,0 +1,13 @@
+-- Leaderkey
+vim.g.mapleader = ' '
+
+-- Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
