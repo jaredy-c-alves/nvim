87664bb2bfbc1dd2e2bfbe9247ca29db839abef0
Jaredy Alves <94395541+jaredy-c-alves@users.noreply.github.com>
2022-10-20 12:12:22 +0100

    Changing lsp formatting command

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -20,7 +20,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
 	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+	vim.keymap.set('n', '<leader>ft', vim.lsp.buf.format, bufopts)
 end
 
 vim.g.coq_settings = { auto_start = 'shut-up' }
